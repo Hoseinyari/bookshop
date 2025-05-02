35bfe4c77d73678a0d8950c65e166fe8f7e0aaf2
Hoseinyari <jwadyary88@gmail.com>
2025-05-03 03:27:11 +0430

    .

--- books/views.py
+++ books/views.py
@@ -16,10 +16,7 @@ from django.contrib.auth.decorators import login_required
 def home_view(request): 
     all_books = Book.objects.all()
     return render(request,'books/home.html',{"all_books": list(all_books)})
+#نمایش جزببات یک کناب
 def book_detail(request):
     the_book = Book.objects.get(book_slug = book_slug)
-@login_required
-#نمایش درامد ها
-def income_view(request): 
-    incomes = Transition.objects.filter(category = 'income')
-    return render(request,'transitions/income.html', {"incomes": list(incomes)}) 
+
