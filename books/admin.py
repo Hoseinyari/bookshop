4fbe374b4b476671f8f77b2d98027d9b8cb1c6c4
Hoseinyari <jwadyary88@gmail.com>
2025-05-21 11:45:46 +0430

    .

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+
+class bookAdmin(admin.ModelAdmin):
+  list_display = ("title", "category", "auther","price")
+  slug = {"slug": ("title", "auther")}
+  
+admin.site.register(Book, bookAdmin)
