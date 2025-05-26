ca6169393769186b8ea50909c8d2c9675a0d3619
Hoseinyari <jwadyary88@gmail.com>
2025-05-27 04:08:29 +0430

    .

--- books/admin.py
+++ books/admin.py
@@ -3,7 +3,7 @@ from .models import Book
 # Register your models here.
 
 class bookAdmin(admin.ModelAdmin):
-  list_display = ("title", "category", "auther","price")
+  list_display = ("Book_title", "Book_category", "Book_auther","Book_price")
   slug = {"slug": ("title", "auther")}
   
 admin.site.register(Book, bookAdmin)
