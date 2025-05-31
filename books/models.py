67ce6f225391701ceb944f529000a6e6bef0c322
Hoseinyari <jwadyary88@gmail.com>
2025-05-31 13:08:49 +0430

    .

--- books/models.py
+++ books/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
-    cat_name = models.CharField(max_length=255,unique=True)
-    cat_slug = models.SlugField() 
+
+categories = {
+    "fiction":"تخیلی" ,
+    "non-fiction":"داستان واقعی",
+    "biography":"بیوگرافی",
+    "science":"علمی",
+    "history":"تاریخی",
+    "fantasy":"فانتزی",
+    "romance":"رومانتیک",
+    "mystery":"رازالود",
+    "children":"مناسب کودکان",
+    "other":"موضوعات دیگر"
+    }
 
 
 
@@ -11,7 +21,7 @@ class Book(models.Model):
     Book_title = models.CharField(max_length=255)
     book_image = models.ImageField()
     book_slug = models.SlugField(max_length=100, unique=True)
-    Book_category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    Book_category = models.CharField(max_length=255,choices =categories)
     Book_auther = models.CharField(max_length=255)
     Book_price = models.PositiveIntegerField()
 
