fc99f8c853230ecdd0eca357f8012d167e470355
Hoseinyari <jwadyary88@gmail.com>
2025-06-03 18:39:12 +0430

    .

--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 categories = {
@@ -20,10 +20,15 @@ categories = {
 class Book(models.Model):
     Book_title = models.CharField(max_length=255)
     book_image = models.ImageField()
-    book_slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     Book_category = models.CharField(max_length=255,choices =categories)
     Book_auther = models.CharField(max_length=255)
     Book_price = models.PositiveIntegerField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.Book_title)
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.Book_title
--- books/urls.py
+++ books/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path("<str:slug>", book_detail, name="book"),
     path("add/", add_book_view, name='add_book'),
     path("search/",search_in_titles, name="search_url"),
-    # path("remove/<str:slug>",delete_view),
     path("remove/",delete_view,name="delete")
 ]
